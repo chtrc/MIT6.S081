ad2866b27c2adef067e6f00e366af5bae0d2b38b
ch <1392620585@qq.com>
2021-10-18 18:14:00 +0800

    lab3

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,26 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void _vmprint(pagetable_t pagetable, int level)
+{
+  for(int i=0; i < 512; ++i) {
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V) {
+      int c = level;
+      while(c){
+        printf("..");
+        if(c > 1) printf(" ");
+        --c;
+      }
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+      if(level < 3) _vmprint((pagetable_t)PTE2PA(pte), level + 1);
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  _vmprint(pagetable, 1);
+}
